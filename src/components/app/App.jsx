f4a1e584b484f902d2db40296f23b93a963eea1b
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-09 10:41:57 +0100

    Image is now visible and redirects to the home

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import React, {  } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Footer from '../footer/Footer'
+import Login from '../login/Login'
 import Navbar from '../navbar/Navbar'
 
 import './App.scss'
@@ -9,6 +10,7 @@ export default function App() {
     return(
         <Router>
             <Navbar/>
+            <Route path='/login' component={Login}/>
             <Footer/>
         </Router>
     )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,21 +2,22 @@ import React, {} from 'react'
 import { Link } from 'react-router-dom'
 
 import './Navbar.scss'
-
+import beard from '../../img/beard.svg'
 
 export default function Navbar() {
     return(
         <nav className="navWrapper">
             <div className="navImgWrapper">
-                <img src="../../img/beard.svg" alt=""/>
-                <span>Img</span>
+                <Link to="/"><img src={beard} alt=""/></Link>
+                
+                
             </div>
             <div className="navLinksWrapper">
                 <Link className="navLink">Beard Oil</Link>
                 <Link className="navLink">Oil Parfume</Link>
                 <Link className="navLink">Vitamens</Link>
                 <Link className="navLink">Link 4</Link>
-                <Link className="navLogin">Login</Link>
+                <Link className="navLogin" to="/login">Login</Link>
 
 
             </div>
