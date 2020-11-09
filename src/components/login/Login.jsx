70983a3fe0138e9ffe99770a5b4c88db63c1141f
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-09 14:36:30 +0100

    created the Login form and a redirection to the register page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,25 @@
 import React, {} from 'react'
 
+import './Login.scss'
+import google from '../../img/google.svg'
+import { Link } from 'react-router-dom'
+
 export default function Login() {
     return(
-        <h1>LOGIN</h1>
+        <div className="loginWrapper">
+                <Link to="/register"><p className="register">Register <i className="fas fa-chevron-right" /></p></Link>
+                
+                <h3 className="loginTitle">Login</h3>
+
+            <form className="formWrapper" action="" >
+                <input className="input" type="text" placeholder="Email"/>
+                <input className="input" type="text" placeholder="Password"/>
+                <button>Login</button>
+            </form>
+            <hr/>
+            <div className="socialLogins">
+                <img src={google} className="google"/>
+            </div>
+        </div>
     )
 };
