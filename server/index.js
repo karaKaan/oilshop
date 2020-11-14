83c63cd91e77793ae2ed35148cd31adb1c2e86b1
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-14 07:48:25 +0100

    connected to the postgres db and created the table structure also installed crypt-package to hash the password

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const pool = require('./db');
 
 const port = process.env.PORT || 3000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+// create a user by registering
+app.post("/api/login", async(req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//Login User by login
+
+//
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`Server runs on PORT ${port}`))
