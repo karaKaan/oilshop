e380057555dd446fea1e867129251c9f75e8d8ca
KaanKara <41960384+karaKaan@users.noreply.github.com>
2020-11-14 12:13:30 +0100

    the user can now register and it will be saved into the db also the password is being hashed

--- server/index.js
+++ server/index.js
@@ -1,18 +1,45 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const pool = require('./db');
+const pool = require('./config/dbConfig');
+const crypto = require('bcrypt')
+const saltRounds = 10
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
 // create a user by registering
-app.post("/api/login", async(req, res) => {
+app.post("/register", async(req, res) => {
     try {
+        const {userFirstName} = req.body
+        const {userName} = req.body
         
+        const {address} = req.body
+        
+        const {city} = req.body
+        const {postalCode} = req.body
+        
+        const {email} = req.body
+        const {password} = req.body
+
+        //Speichere den Kunden in die Database
+        crypto.hash(password, saltRounds, async(err, hash) => {
+            try {
+                await pool.query('insert into kunde(vorname, nachname, email, password) values($1,$2,$3,$4)',[userFirstName, userName, email, hash])
+                // Speichere die Adresse 
+                await pool.query(`insert into adresse(straße, fk_kunde) values($1, (select KID from Kunde where email = '${email}'))`, [address])
+                
+                await pool.query(`insert into stadt(stadt, plz, fk_adresse) values($1,$2, 
+                    (select AID from adresse where fk_kunde = (select kid from kunde where email = '${email}')))`, [city, postalCode])
+                
+            } catch (err) {
+                console.log(err)
+            }
+            })
+        res.json('user Inserted')
     } catch (err) {
         console.error(err.message)
     }
--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 
 import './Register.scss';
 
 export default function Register() {
+    const [userFirstName, setUserFirstName] = useState()
+    const [userName, setUserName] = useState()
+    const [address, setAddress] = useState()
+    const [city, setCity] = useState()
+    const [postalCode, setPostalCode] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+
+    async function registerUser() {
+        try {
+            
+            const body = {userFirstName, userName, address, city, postalCode, email, password}
+            
+            await fetch('http://localhost:5000/register', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(body)
+            })
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+
     return(
         <div className="loginWrapper">
         <Link to="/login"><p className="register">Login <i className="fas fa-chevron-right" /></p></Link>
@@ -11,15 +37,15 @@ export default function Register() {
         <h3 className="loginTitle">Register</h3>
 
     <form className="registerFormWrapper" action="" >
-        <input className="input" type="text" placeholder="Firstname*"/>
-        <input className="input" type="text" placeholder="Lastname*"/>
-        <input className="input" type="text" placeholder="Adress+Nr*"/>
-        <input className="input" type="text" placeholder="City*"/>
-        <input className="input" type="text" placeholder="Postal Code*"/>
-        <input className="input" type="text" placeholder="Email*"/>
-        <input className="input" type="text" placeholder="Password*"/>
+        <input className="input" type="text" placeholder="Firstname*" onChange={(e) => setUserFirstName(e.target.value)}/>
+        <input className="input" type="text" placeholder="Lastname*" onChange={(e) => setUserName(e.target.value)}/>
+        <input className="input" type="text" placeholder="Adress+Nr*" onChange={(e) => setAddress(e.target.value)}/>
+        <input className="input" type="text" placeholder="City*" onChange={(e) => setCity(e.target.value)}/>
+        <input className="input" type="text" placeholder="Postal Code*" onChange={(e) => setPostalCode(e.target.value)}/>
+        <input className="input" type="text" placeholder="Email*" onChange={(e) => setEmail(e.target.value)}/>
+        <input className="input" type="password" placeholder="Password*" onChange={(e) => setPassword(e.target.value)}/>
     </form>
-        <button className="successBtn">Register</button>
+        <button className="successBtn" onClick={() => registerUser()}>Register</button>
         <p>* Fields with these symbols are mandatory</p>
 </div>
     )
